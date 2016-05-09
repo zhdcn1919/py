8f3aa95c5cff55107bc44b5cd21dc73604027c7c
hades <207522540@qq.com>
2016-05-10 06:47:35 +0800

    add len fountion

--- tieba.py
+++ tieba.py
@@ -10,7 +10,7 @@ tieba = requests.get("http://tieba.baidu.com/f?kw=庆云")
 
 print tieba.text
 
-fr = open('tieba.txt','w')
+fr = open('src_tieba.txt','w')
 fr.write(tieba.text)
 fr.close()
 
@@ -18,9 +18,14 @@ author = re.findall(u'主题作者: (.*?)"',tieba.text,re.S)
 title = re.findall('class="j_th_tit ">(.*?)</a>',tieba.text,re.S)
 time= re.findall(u'"创建时间">(.*?)</span>',tieba.text,re.S)
 
+def bgetlen(n):
+    str_cut = re.findall('[0-9a-zA-Z]',n,re.S)
+    str2 = len(str_cut)/2
+    alen = len(n)-len(str_cut)+str2
+    return alen
+
 for (c,a,t) in zip(time,author,title):
-    if(len(a)<4):
-        a = a+'       '
-        print c+'\t'+a+"  \t"+t
+    if(bgetlen(a)<4):
+        print c+'\t'+a+'  \t'+t
     else:
-        print c+"\t"+a+'\t'+t
+        print c+'\t'+a+'\t'+t
